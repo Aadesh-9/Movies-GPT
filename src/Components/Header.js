7959b23c707535566e7e7bb55f2b085211681ff0
Aadesh-9 <aadeshwasamkar@gmail.com>
2024-02-07 10:05:04 +0530

    added SignIn SignOut With authentication & and validations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../Utils/firebase";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const photoClickHandler = () => {
+    setIsClicked(!isClicked);
+  };
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="absolute w-screen  p-3  bg-gradient-to-b from-black flex justify-between">
+      <img
+        className=" w-44 "
+        alt="netflix-logo"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+      ></img>
+      {user && (
+        <div className="flex p-2 relative right-20">
+          <img
+            onClick={photoClickHandler}
+            className="w-16 h-16 rounded-[50%] relative left-20"
+            alt="user-icon"
+            src={user?.photoURL}
+          ></img>
+          {isClicked && (
+            <button
+              onClick={handleSignOut}
+              className="w-[80px] h-[40px] relative top-[68px] left-[8px] mt-1  p-1 rounded-md bg-red-600 font-bold text-white cursor-pointer"
+            >
+              Sign Out
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
